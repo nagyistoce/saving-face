5bf761064188ceff398130e3c55150b848167fc8
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-09-28 22:30:37 +0000

    Added Include Paths For src, and include, and moved the files.

--- trunk/ saving-face --username 1900Geek@gmail.com/sf_tr_func.h
+++ trunk/ saving-face --username 1900Geek@gmail.com/sf_tr_func.h
@@ -5,10 +5,13 @@
 namespace SF
 {
 	
+	//This Function will Ideally have its inputs constructed by the SDK
 	SF_TR_MATRIX calculateTRMatrix(const PXCPoint3DF32 rwcReference, const SF_YPR ypr);
 
+	//Take a Raw 3D Coord and put orient it, return its Model space address.
 	SF_MODEL_COORD3D transformCoord(const PXCPoint3DF32 realWorldCoord, const SF_TR_MATRIX);
 
+	//Check to make sure a coord is within the bounds of model space
 	SF_STS coordInModelSpace(const SF_MODEL_COORD3D mdsCoord, const Model *model);
 
 }
