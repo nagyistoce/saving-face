cee0d4e61248f210730906c91f0129a08f5c485a
aemason@alaska.edu <aemason@alaska.edu@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-10-30 21:17:20 +0000

    Implemented calculateTRMatrix in sf_tr_func.cpp

---  saving-face --username 1900Geek@gmail.com/sf_tr_func.cpp
+++  saving-face --username 1900Geek@gmail.com/sf_tr_func.cpp
@@ -6,7 +6,28 @@ namespace SF
 	//Pass in the nose coord, and the Yaw, Pitch, and Roll
 	SF_TR_MATRIX* calculateTRMatrix(const SF_R3_COORD &rwcReference, const SF_YPR &ypr)
 	{
-		return NULL;
+		SF_SCALAR tx[9] = {1,0,0,0,cos(ypr.yaw),-sin(ypr.yaw),0,sin(ypr.yaw),cos(ypr.yaw)};
+		SF_SCALAR ty[9] = {cos(ypr.pitch),0,sin(ypr.pitch),0,1,0,-sin(ypr.pitch),0,cos(ypr.pitch)};
+		SF_SCALAR tz[9] = {cos(ypr.roll),-sin(ypr.roll),0,sin(ypr.roll),cos(ypr.roll),0,0,0,1};
+
+		SF_SCALAR temp[9] = {tx[0] * ty[0] + tx[1]*ty[3] + tx[2] * ty[6], tx[0] * ty[1] + tx[1]*ty[4] + tx[2] * ty[7], tx[0] * ty[2] + tx[1]*ty[5] + tx[2] * ty[8],
+						     tx[3] * ty[0] + tx[4]*ty[3] + tx[5] * ty[6], tx[3] * ty[1] + tx[3]*ty[3] + tx[5] * ty[7], tx[3] * ty[2] + tx[4]*ty[5] + tx[5] * ty[8],
+						     tx[6] * ty[0] + tx[7]*ty[3] + tx[8] * ty[6], tx[6] * ty[1] + tx[7]*ty[4] + tx[8] * ty[7], tx[6] * ty[2] + tx[7]*ty[5] + tx[8] * ty[8]};
+
+
+		SF_SCALAR tmMatrix[9] = {temp[0] * tz[0] + temp[1]*tz[3] + temp[2] * tz[6], temp[0] * tz[1] + temp[1]*tz[4] + temp[2] * tz[7], temp[0] * tz[2] + temp[1]*tz[5] + temp[2] * tz[8],
+									temp[3] * tz[0] + temp[4]*tz[3] + temp[5] * tz[6], temp[3] * tz[1] + temp[3]*tz[3] + temp[5] * tz[7], temp[3] * tz[2] + temp[4]*tz[5] + temp[5] * tz[8],
+									temp[6] * tz[0] + temp[7]*tz[3] + temp[8] * tz[6], temp[6] * tz[1] + temp[7]*tz[4] + temp[8] * tz[7], temp[6] * tz[2] + temp[7]*tz[5] + temp[8] * tz[8]};
+
+		SF_TR_MATRIX tm = {0};
+
+		tm.trXYZ.x -= rwcReference.x;
+		tm.trXYZ.y -= rwcReference.y;
+		tm.trXYZ.z -= rwcReference.z;
+
+		memcpy(tm.trMatrix, tmMatrix, 9);
+
+		return &tm;
 	}
 
 	//So the Actual Transformation
