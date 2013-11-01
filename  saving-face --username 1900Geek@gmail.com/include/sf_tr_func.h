61abb33613fed4bb5ef77a805e73ccc3953ce237
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-11-01 04:45:12 +0000

    Updated sf_tr_func.h comments.

---  saving-face --username 1900Geek@gmail.com/include/sf_tr_func.h
+++  saving-face --username 1900Geek@gmail.com/include/sf_tr_func.h
@@ -14,8 +14,10 @@ namespace SF
 	//Calculated once per frame
 	void calculateTRMatrix(SF_TR_MATRIX &tm, SF_R3_COORD &rwcReference, const SF_YPR &ypr);
 
+	//Apply linear translation to a vector
 	inline void translateCoord(SF_R3_COORD &model_Coord3d, const SF_R3_COORD &origCoord,const SF_TR_MATRIX &tr_matrix);
 	
+	//Apply rotational transform to a vector.
 	inline void rotateCoord(SF_MODEL_COORD3D &model_r3_coord,const SF_R3_COORD &tr_Coord3d, const SF_TR_MATRIX &tr_matrix);
 
 	//So the Actual Transformation
@@ -48,7 +50,6 @@ namespace SF
 		SF_SCALAR ty[9] = {cos(ypr.pitch),0.f,sin(ypr.pitch),0.f,1.f,0.f,-sin(ypr.pitch),0.f,cos(ypr.pitch)};
 		SF_SCALAR tz[9] = {cos(ypr.roll),-sin(ypr.roll),0.f,sin(ypr.roll),cos(ypr.roll),0.f,0.f,0.f,1.f};
 
-		//Updated tx[3] * ty[3] to read tx[4]*ty[4]... Great job though andy this must have been hard to keep track of.
 		SF_SCALAR temp[9] = {tx[0] * ty[0] + tx[1]*ty[3] + tx[2] * ty[6], tx[0] * ty[1] + tx[1]*ty[4] + tx[2] * ty[7], tx[0] * ty[2] + tx[1]*ty[5] + tx[2] * ty[8],
 						     tx[3] * ty[0] + tx[4]*ty[3] + tx[5] * ty[6], tx[3] * ty[1] + tx[4]*ty[4] + tx[5] * ty[7], tx[3] * ty[2] + tx[4]*ty[5] + tx[5] * ty[8],
 						     tx[6] * ty[0] + tx[7]*ty[3] + tx[8] * ty[6], tx[6] * ty[1] + tx[7]*ty[4] + tx[8] * ty[7], tx[6] * ty[2] + tx[7]*ty[5] + tx[8] * ty[8]};
@@ -67,8 +68,8 @@ namespace SF
 		rotateCoord(mdl_coord, r3_Coord, tr_matrix);
 	}
 	
-	//Transforms the Coord into indexed model coords
-	//Returns null if outside model space.
+
+
 	inline void coordInModelSpace(SF_MODEL_COORD3D_INDEX &returnIndexed, const SF_MODEL_COORD3D &md_Coord, const Model::Model_Info *model){
 		SF_MODEL_COORD3D_INDEX *returnIndex = new SF_MODEL_COORD3D_INDEX;
 		returnIndexed.x = floor((md_Coord.x - model->xMin) / model->deltaX);
@@ -76,7 +77,7 @@ namespace SF
 		returnIndexed.z = floor((md_Coord.z - model->zMin) / model->deltaZ);
 	}
 
-	//Apply the translation to a coord
+
 	inline void translateCoord(SF_R3_COORD &model_Coord3d, const SF_R3_COORD &origCoord,const SF_TR_MATRIX &tr_matrix){
 		model_Coord3d.x = origCoord.x - tr_matrix.trV.x;
 		model_Coord3d.y = origCoord.y - tr_matrix.trV.y;
