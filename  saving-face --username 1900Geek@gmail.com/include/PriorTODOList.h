053fc12b547c162383f1f52e108262985ce95332
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-11-17 21:40:38 +0000

    Updated TODO List. Lets get the high priority tests done. So that we can just be done with them. Then lets focus on the db, and then model_builder/sdk session code.

---  saving-face --username 1900Geek@gmail.com/include/PriorTODOList.h
+++  saving-face --username 1900Geek@gmail.com/include/PriorTODOList.h
@@ -1,19 +1,26 @@
 /*
 *Priority number if assigned, followed by the TODO
 * Class sf_db
-* SF_STS saveDatabase(string path);
-* SF_STS loadDatabase(string path);
-* SF_STS deleteModelFromDatabase(SF_MUID *modelID);
+* (High) SF_STS saveDatabase(string path);
+* (High) SF_STS loadDatabase(string path);
+* (Low)  SF_STS deleteModelFromDatabase(SF_MUID *modelID);
 * 
 * I will add more as I go through, but these are good to start on.
+* 
+* TESTS TODO
+* (Medium) TEST_METHOD(constructModelArrayTest) Quick and easy
+* (High)   TEST_METHOD(TestFileIOStreams) Just need to test UID's and Model Params, Quick and Easy
+* (Medium) TEST_METHOD(TestModelArrModifyAndRead)
+* (High)   TEST_METHOD(testVectorRotation) See comments, pretty quick but important to know it works
+* (High)   TEST_METHOD(testVectorTransform) See comments, pretty quick but important to know it works
+* (Low)    TEST_METHOD(fullySpecifiedConstructorTest)
 *
-*
-*
-*
-*
-*
-*
-*
-*
-*
+* Class Model
+* Add params to keep track of comparison tracking. (Figure out the params later)
+* 
+* Class Model_Builder
+* Un-implemented methods... High priority
+* Actually build the model... High priority...
+* Save Video Option... (to be implemented in sf_session first)
+* Save Snapshot... (to be implemented in sf_session first) 
 */
