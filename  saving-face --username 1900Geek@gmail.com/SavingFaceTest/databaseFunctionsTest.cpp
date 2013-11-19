08fe65686eb3e7bd5f43654f261f20e86706c486
aemason@alaska.edu <aemason@alaska.edu@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-11-19 20:43:05 +0000

    debugging databaseFunctionsTest.cpp

---  saving-face --username 1900Geek@gmail.com/SavingFaceTest/databaseFunctionsTest.cpp
+++  saving-face --username 1900Geek@gmail.com/SavingFaceTest/databaseFunctionsTest.cpp
@@ -17,44 +17,55 @@ namespace SavingFaceTest
 			sf_db *db = new sf_db;
 			Model *modelOne, *modelTwo, *modelThree;
 			modelOne = new Model("MD.","Punish","Me", "Please", "Sr", "Male","Owww@drPain.com");
-			modelTwo = new Model("Mr.","Joe","F", "Dirt", "Jr", "Male","DirtJoe@email.com");
-			modelThree = new Model("Dr.","Gordon","P", "Freeman", "I", "Male","gordon@valve.com");
+			//modelTwo = new Model("Mr.","Joe","F", "Dirt", "Jr", "Male","DirtJoe@email.com");
+			//modelThree = new Model("Dr.","Gordon","P", "Freeman", "I", "Male","gordon@valve.com");
 
 			modelOne->setPersonUID(111111L);
 			modelOne->setModelUID(222222L); 
 			SF_MUID expMUIDOne = modelOne->getModelUID();
 
-			modelTwo->setPersonUID(333333L);
+			/*modelTwo->setPersonUID(333333L);
 			modelTwo->setModelUID(444444L); 
 			SF_MUID expMUIDTwo = modelOne->getModelUID();
 
 			modelThree->setPersonUID(555555L);
 			modelThree->setModelUID(666666L); 
 			SF_MUID expMUIDThree = modelOne->getModelUID();
-
+			*/
 			string expConcatNameOne = modelOne->getConcatenatedName();
-			string expConcatNameTwo = modelTwo->getConcatenatedName();
-			string expConcatNameThree = modelThree->getConcatenatedName();
+			//string expConcatNameTwo = modelTwo->getConcatenatedName();
+			//string expConcatNameThree = modelThree->getConcatenatedName();
+
+			modelOne->initModelArray();
+			
+			modelOne->getModelInfo()->modelArr[0] = 'A';
+			modelOne->getModelInfo()->modelArr[1] = 'B';
+			modelOne->getModelInfo()->modelArr[2] = 'C';
+
+			char str[200];   
+			sprintf_s(str, 200, "Model UID = %d\n",  modelOne->getModelUID());
+			Logger().WriteMessage(str);
+
 
 			if (db->addModelToDatabase(modelOne))
 				Logger().WriteMessage(L"Model one added to database.");
 			else
 				Logger().WriteMessage(L"Model one failed adding to database.");
 			
-			db->addModelToDatabase(modelTwo);
-			db->addModelToDatabase(modelThree);
+			//db->addModelToDatabase(modelTwo);
+			//db->addModelToDatabase(modelThree);
 
 			delete modelOne;
-			delete modelTwo;
-			delete modelThree;
+			//delete modelTwo;
+			//delete modelThree;
 
 			modelOne = 0;
-			modelTwo = 0;
-			modelThree = 0;
+			//modelTwo = 0;
+			//modelThree = 0;
 
 			modelOne = new Model();
-			modelTwo = new Model();
-			modelThree = new Model();
+			//modelTwo = new Model();
+			//modelThree = new Model();
 
 			modelOne = db->getModel(expMUIDOne);
 
@@ -65,20 +76,22 @@ namespace SavingFaceTest
 				Logger().WriteMessage(L"\nModel one retrieved from database.");
 				Logger().WriteMessage(modelOne->getFirstName().c_str());
 			}
-
+ 
+			sprintf_s(str, 200, "Model UID = %d\n",  modelOne->getModelUID());
+			Logger().WriteMessage(str);
 			
 
-			modelTwo = db->getModel(expMUIDTwo);
-			modelThree = db->getModel(expMUIDThree);
+			//modelTwo = db->getModel(expMUIDTwo);
+			//modelThree = db->getModel(expMUIDThree);
 
-			Assert().IsTrue(expMUIDOne == modelOne->getModelUID(),L"MUID is not equal");
+			Assert().AreEqual(expMUIDOne, modelOne->getModelUID(),L"MUID is not equal");
 			Assert().AreEqual(expConcatNameOne, modelOne->getConcatenatedName(),L"Names are not equal");
-			Assert().AreEqual(expConcatNameTwo, modelTwo->getConcatenatedName(),L"Names are not equal");
-			Assert().AreEqual(expConcatNameThree, modelThree->getConcatenatedName(),L"Names are not equal");
+			//Assert().AreEqual(expConcatNameTwo, modelTwo->getConcatenatedName(),L"Names are not equal");
+			//Assert().AreEqual(expConcatNameThree, modelThree->getConcatenatedName(),L"Names are not equal");
 
 			delete modelOne;
-			delete modelTwo;
-			delete modelThree;
+			//delete modelTwo;
+			//delete modelThree;
 
 			delete db;
 
