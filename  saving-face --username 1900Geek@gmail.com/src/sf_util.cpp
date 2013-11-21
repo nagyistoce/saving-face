f5f2a536d6579fdc78dc10f710d4f24ca454a584
aemason@alaska.edu <aemason@alaska.edu@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-11-21 22:51:51 +0000

    Updated functions in sf_util.cpp

---  saving-face --username 1900Geek@gmail.com/src/sf_util.cpp
+++  saving-face --username 1900Geek@gmail.com/src/sf_util.cpp
@@ -1,45 +1,16 @@
 #include "sf_util.h"
 #include <windows.h>
 
-//should take an argument and return
-//Working directory + "\\partialPath\\"
-string sf_util::getFullPath()
-{
-	//Should calculate the path
-	return path;
-}
-
-//Would rewrite to include partial path...
-//Ex makeDirectory("database")
-//That way we can have seperate directories for testing and actual use.
-//Multiple Databases.
-//Should have a wrapper call to getFull("database") that returns the path.
-//No need to store the path.
-void sf_util::makeDirectory()
+//Appends the partial path onto the current working directory
+string sf_util::getFullPath(string partialPath)
 {
 	wchar_t* buffer;
 
-	// Gets the current working directory  
+	//gets current working directory
 	buffer = _wgetcwd( NULL, 0 );
-			
-	//append full file path onto temp
+
 	wchar_t temp[200] = L"";
 	wcscat_s(temp, buffer);
-	wcscat_s(temp, L"\\database\\");//Hard-coding is bad practice.
-	//Instead call getFullPath(partialPath);
-
-	DWORD ftyp = GetFileAttributesW(temp);
-	delete[] buffer;
-
-	//Creates a folder if it does not exist
-	if (ftyp == INVALID_FILE_ATTRIBUTES)
-	{
-		_wmkdir(temp);
-	}
-	else if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
-	{
-		//this already is a directory
-	}
 
 	//convert from wide char to narrow char array
 	char ch[200];
@@ -49,11 +20,38 @@ void sf_util::makeDirectory()
 	//A std:string  using the char* constructor.
 	std::string ss(ch);
 
-	//store string to private path variable for later acces
-	path = ss;//No need to store.
-	//However making a global macro in sf_defs for DEFAULT_DB_PATH is fine.
-	//Probably add one for a testing path as well.
+	ss += "\\" + partialPath +"\\";
 
-	delete temp;
-	delete ch;
+	delete[] buffer;
+	return ss;
+}
+
+//Appends the partial path onto the current working directory
+void sf_util::makeDirectory(string partialPath)
+{
+			
+	string path = getFullPath(partialPath);
+
+	//Convert string to LPCWSTR
+	int len;
+    int slength = (int)path.length() + 1;
+    len = MultiByteToWideChar(CP_ACP, 0, path.c_str(), slength, 0, 0); 
+    wchar_t* buffer = new wchar_t[len];
+    MultiByteToWideChar(CP_ACP, 0, path.c_str(), slength, buf, len);
+    LPCWSTR lpcwPath(buf);
+    delete[] buffer;
+
+	DWORD ftyp = GetFileAttributesW(lpcwPath);
+
+	//Creates a folder if it does not exist
+	if (ftyp == INVALID_FILE_ATTRIBUTES)
+	{
+		_wmkdir(lpcwPath);
+	}
+	else if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
+	{
+		//this already is a directory
+	}
+
+	delete[] lpcwPath;
 }
