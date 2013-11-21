6a649dd4c13d0b3635f2618f35eed6aa1f60b24d
aemason@alaska.edu <aemason@alaska.edu@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-11-21 23:13:02 +0000

    Updated PriorTODOList.h

---  saving-face --username 1900Geek@gmail.com/include/PriorTODOList.h
+++  saving-face --username 1900Geek@gmail.com/include/PriorTODOList.h
@@ -1,18 +1,18 @@
 /*
 *Priority number if assigned, followed by the TODO
 
-***** Make sf_util.cpp
-* Extract the code used in databaseFunctionsTest to make functions
-string getFullPath(string partialPath)
-string makeDirectory(string Path)
-
-
 * Class sf_sdk_session
 * (High) Method to get depth vertices data
 * (High) Method to convert YPR to a depth vertice
 * (Medium) Save video
 * (Medium) Save Snapshot
 * I will add more as I go through, but these are good to start on.
+*
+* Class Model_Builder
+* Un-implemented methods... High priority (Most depend on sf_sdk functionality)
+* Actually build the model... High priority...
+* Save Video Option... (to be implemented in sf_session first)
+* Save Snapshot... (to be implemented in sf_session first)... Save the image of the last frame used to build.
 * 
 * TESTS TODO
 *
@@ -20,14 +20,9 @@ string makeDirectory(string Path)
 * Add params to keep track of comparison tracking. (Figure out the params later)
 * Add return fail status and do not save, if initModelArr has not been called.
 * 
-* Class Model_Builder
-* Un-implemented methods... High priority (Most depend on sf_sdk functionality)
-* Actually build the model... High priority...
-* Save Video Option... (to be implemented in sf_session first)
-* Save Snapshot... (to be implemented in sf_session first)... Save the image of the last frame used to build.
-
 
 *Classes that are Finished and Tested
 *sf_tr_func (Well Should add more testing for modelspace coords... But should become self-evident if there is a problem).
 *sf_db (I feel confident this is done -Andy)
+*sf_util (this class is tested in testDatabaseFileIOStreams and works)
 */
