036e5510944421b93640dc6732643313ad327e0d
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-11-22 03:58:34 +0000

    Added controller class to attach UI. Will be the sole source for program logic flow.

---
+++ trunk/ saving-face --username 1900Geek@gmail.com/sf_controller.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "sf_db.h"
+#include "sf_model.h"
+#include "sf_sdk_session.h"
+#include "sf_model_builder.h"
+
+using namespace SF;
+using namespace std;
+class sf_controller
+{
+	//This class will be the one that the GUI attaches to
+	//This class will own all instances of the primary components
+	//And contains the flow control logic.
+
+public:
+	sf_controller(void);
+	~sf_controller(void);
+	
+	//Contains functionality to parse a text command.
+	SF_STS command(string textCommand);
+
+	//Initialization outside of constructor, means constructor will never fail.
+	//If mode = "", default initialization
+	//Set up macros for other special modes.
+	//Debug, verbose, alternative algorithms, playback mode(no camera)... etc
+	SF_STS init(string mode = "");
+
+	//Add Functions as needed
+
+private: 
+	sf_db *db;
+	sf_model_builder *mBuilder;
+	SF_Session *session;
+};
+
