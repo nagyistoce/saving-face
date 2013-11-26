ec140fb2e862111f51a28063c6029a5e39af0450
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-11-26 04:18:18 +0000

    Updated TODO

---  saving-face --username 1900Geek@gmail.com/include/PriorTODOList.h
+++  saving-face --username 1900Geek@gmail.com/include/PriorTODOList.h
@@ -2,16 +2,12 @@
 *Priority number if assigned, followed by the TODO
 
 * Class sf_sdk_session
-* (Medium) Save video
 * (Medium) Save Snapshot
 
 
 *
 * Class Model_Builder
-* Implement functions that recieve YPR and Nose/calculate and save the Tr_Matrix to a local pointer.
 * Make a process vertice method... and build the model. (Major milestone).
-* Un-implemented methods... High priority (Most depend on sf_sdk functionality)
-* Save Video Option... (to be implemented in sf_session first)
 * Save Snapshot... (to be implemented in sf_session first)... Save the image of the last frame used to build.
 * 
 * TESTS TODO
@@ -19,10 +15,19 @@
 * Class Model
 * Add params to keep track of comparison tracking. (Figure out the params later)
 * Add return fail status and do not save, if initModelArr has not been called.
-* 
 
 *Classes that are Finished and Tested
 *sf_tr_func (Well Should add more testing for modelspace coords... But should become self-evident if there is a problem).
 *sf_db (I feel confident this is done -Andy)
 *sf_util (this class is tested in testDatabaseFileIOStreams and works)
+*sf_tr_test (exist as a wrapper call to the tr_func for test dll.)
+
+*classes to be worked on.
+*sf_controller, contains main program flow and holds the globals. Interfaces with UI.
+*sf model builder... Fully constructs the model, responsible to make sure all neccesary vars are filled.
+*	Will also be the fondation for the comparitor... consider moving globals/global functions to own cpp file.
+*	That is a matter of making a header/cpp file and a #include.
+*	Note that the implementation should be in the cpp file so it is compiled to the .lib for testing.
+*sf_comparitor... Not implemented, finish model_builder first. (can be implemented PDQ)
+*	Allow for multiple instances.
 */
