9e2443b717a621b02307ffe2edb04581aa394796
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-11-27 22:13:47 +0000

    SaveImage Updated. Added sourced_util.h (for borrowed code). And a little directory house keeping. Still cleaning that up.

--- trunk/ saving-face --username 1900Geek@gmail.com/SavingFaceTest/controllerTest.cpp
+++ trunk/ saving-face --username 1900Geek@gmail.com/SavingFaceTest/controllerTest.cpp
@@ -1,17 +1,36 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "sf_sdk_session.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace SavingFaceTest
 {
+	bool shutterTest()
+	{
+		static int i = 0;
+		return (++i == 80);
+	}
+	bool photoFinish()
+	{
+		static int i = 0;
+		return (++i == 100);
+	}
+
 	TEST_CLASS(controllerTest)
 	{
 	public:
 		
 		TEST_METHOD(savePhoto)
 		{
-			
+			SF_Session session;
+			session.createSession();
+			session.setOptions(NULL,NULL);
+			Model model;
+			model.setName("Mr.","Fancy","","Pants","");
+			session.snapshotLoop(&model,&shutterTest,&photoFinish);
+
+			Assert().Fail(L"Not Finished");
 		}
 
 	};
---
+++ trunk/ saving-face --username 1900Geek@gmail.com/include/sf_controller.h
@@ -0,0 +1,58 @@
+#ifndef _SF_CONTROLLER
+#define _SF_CONTROLLER
+//Something strange is going on with the includes.
+#include <windows.h>
+#include "sf_db.h"
+#include "sf_model.h"
+#include "sf_sdk_session.h"
+#include "sf_model_builder.h"
+
+using namespace SF;
+using namespace std;
+class sf_controller
+{
+	//This class will be the one that the GUI attaches to
+	//This class will own all instances of the primary components
+	//And contains the flow control logic.
+
+public:
+	sf_controller(void);
+	~sf_controller(void);
+	
+	//Return -1 if required fields are not initialized.
+	SF_MUID createModelModel(SF_NAME const salutation, SF_NAME const firstName, SF_NAME const middleName, SF_NAME const lastName, SF_NAME const suffix,SF_GENDER const gender,  SF_EMAIL const email);
+	
+
+	//Opens a video feed to take the picture.
+	SF_STS takeSnapshot();
+
+	//Take the photo
+	string pressShutter(SF_MUID modelId);
+	
+	//Close the Snapshot video feed
+	SF_STS snapshotFinished();
+
+	string getSnapshotPath(SF_MUID modelID);
+
+	SF_STS buildModel(SF_MUID modelID);
+	
+	
+	
+	//Contains functionality to parse a text command.
+	//Mostly for debug
+	SF_STS command(string textCommand);
+
+	//Initialization outside of constructor, means constructor will never fail.
+	//If mode = "", default initialization
+	//Set up macros for other special modes.
+	//Debug, verbose, alternative algorithms, playback mode(no camera)... etc
+	SF_STS init(string mode = "");
+
+	//Add Functions as needed
+
+private: 
+	sf_db *db;
+	sf_model_builder *mBuilder;
+	SF_Session *session;
+};
+#endif
