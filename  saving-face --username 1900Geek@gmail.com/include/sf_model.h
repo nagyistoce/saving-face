9330d2bd5dbc8ec611b0abdd35b401c284d58ef8
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-12-04 08:51:07 +0000

    Small Algorithm Upgrade. Among other things I increased the model building algorithm to 1000 frames, and occluded any points that do not receive at least 20 hits. This produces Highly accurate models by omitting bad data. Which will help significantly in the next phase.

---  saving-face --username 1900Geek@gmail.com/include/sf_model.h
+++  saving-face --username 1900Geek@gmail.com/include/sf_model.h
@@ -9,6 +9,7 @@ namespace SF
 	//Define Defaults
 	//Tweak here to manipulate the default configuration
 	//Will not affect previous instances
+	//NOTE::DEAL STRICTLY WITH PERFECT POWERS OF 2
 	#define SF_DEFAULT_XMIN (-0.125f)
 	#define SF_DEFAULT_XMAX (0.125f)
 	#define SF_DEFAULT_YMIN (-0.125f)
@@ -17,7 +18,7 @@ namespace SF
 	#define SF_DEFAULT_ZMAX (0.20f)
 	#define SF_DEFAULT_DELTAX (0.0025f)
 	#define SF_DEFAULT_DELTAY (0.0025f)
-	#define SF_DEFAULT_DELTAZ (0.0025f)
+	#define SF_DEFAULT_DELTAZ (0.00125f)
 	
 	class Model
 	{
