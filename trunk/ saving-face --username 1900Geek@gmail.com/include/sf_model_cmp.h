8601be20f63a5b4dc0f8fb114ea5a792482a12b5
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-12-09 22:15:24 +0000

    Added skeleton implementation of the compare module.

---
+++ trunk/ saving-face --username 1900Geek@gmail.com/include/sf_model_cmp.h
@@ -0,0 +1,34 @@
+#include "sf_defs.h"
+#include "sf_model.h"
+#include "sf_db.h"
+#include "sf_model_builder.h"
+
+
+using namespace SF;
+namespace SF
+{	
+	class sf_model_cmp
+	{
+	public:
+		
+		struct cmp_results
+		{
+			string *names;
+			float *scores;
+			string *imagePaths;
+		};
+
+		sf_model_cmp(sf_db *db, SF_Session *session);
+		~sf_model_cmp();
+
+		//temp until call back is added
+		cmp_results *compare();
+		
+
+
+	private:
+		sf_db *db;
+		Model *model;
+		SF_Session *session;
+	};
+}
