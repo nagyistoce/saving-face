4275b7eed6ba5717f77f97b3bde98e0d0e68d8b5
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-12-09 23:01:28 +0000

    Working on the compare algorithm.

---  saving-face --username 1900Geek@gmail.com/include/sf_model_cmp.cpp
+++  saving-face --username 1900Geek@gmail.com/include/sf_model_cmp.cpp
@@ -6,17 +6,33 @@ namespace SF
 {
 		sf_model_cmp::sf_model_cmp(sf_db *db, SF_Session *session)
 		{
-
+			this->db = db;
+			this->session = session;
+			this->builder = new sf_model_builder();
+			models = 0;
 		}
 		sf_model_cmp::~sf_model_cmp()
 		{
-
+			delete builder;
 		}
 
 		//temp until call back is added
 		sf_model_cmp::cmp_results *sf_model_cmp::compare()
 		{
-
+			//A First Crude Implementation
+			SF_MUID muid = builder->addNewModel();
+			SF_STS sts = builder->buildModel(muid,session,false);
+			model = builder->getModel(muid);
+			SF_MODEL_ARR arr = model->getReadOnlyModelArr();
 			return 0;
+		}
+
+		SF_STS sf_model_cmp::getModelReferences()
+		{
+			if(models)
+				delete[] models;
+			models = new Model*[50];
+			db->getModelList(models,numModels);
+			return SF_STS_FAIL;
 		}
 }
