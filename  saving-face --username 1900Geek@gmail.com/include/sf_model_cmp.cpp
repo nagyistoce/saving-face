e7480ee08abe32489c76980b5210bc0488be210a
1900Geek@gmail.com <1900Geek@gmail.com@48930643-50f4-79ac-576b-c2fbae979fe4>
2013-12-09 22:15:24 +0000

    Added skeleton implementation of the compare module.

---
+++  saving-face --username 1900Geek@gmail.com/include/sf_model_cmp.cpp
@@ -0,0 +1,22 @@
+#include "sf_model_cmp.h"
+
+
+using namespace SF;
+namespace SF
+{
+		sf_model_cmp::sf_model_cmp(sf_db *db, SF_Session *session)
+		{
+
+		}
+		sf_model_cmp::~sf_model_cmp()
+		{
+
+		}
+
+		//temp until call back is added
+		sf_model_cmp::cmp_results *sf_model_cmp::compare()
+		{
+
+
+		}
+}
